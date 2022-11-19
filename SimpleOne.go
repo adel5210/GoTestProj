7213ab84cfe5d9432badbf567fababcdd6ac0b9a
adel <adel.albediwy@gmail.com>
2022-11-19 21:02:09 +0300

    Second commit

--- SimpleOne.go
+++ SimpleOne.go
@@ -3,11 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Start","Here")
+	fmt.Println("Start", "Here")
 	a := true
 	fmt.Println(a)
 
-	for i:=1; i<=10; i++{
-		fmt.Println(i)
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+
+		if j := i; j == 10 {
+			fmt.Println("Its 10")
+		}
 	}
+
+	testSwitch := 'a'
+	switch testSwitch {
+	case 'a':
+		fmt.Println('a')
+	case 'b':
+		fmt.Println('b')
+
+	}
+
+	testType := func(i interface{}) {
+		switch t := i.(type) {
+		default:
+			fmt.Println(t)
+		}
+	}
+	testType("a")
+	arr := make([]int, 1)
+	mp := make(map[int]string)
+	for i := 0; i < 3; i++ {
+		arr = append(arr, i)
+		mp[i] = "yes"
+	}
+
 }
